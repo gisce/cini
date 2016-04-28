2c6880224b12a0e8ed316c4d188dc1bd706cecda
Eduard Carreras <ecarreras@gisce.net>
2016-04-28 16:53:40 +0200

    Add dummy install package

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import setuptools
+
+
+setuptools.setup(
+    name='cini',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/gisce/cini',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description="Llibreria per calcular de forma automàtica el codi "
+                "d'identificació normalitzat d'instal·lacions"
+)
