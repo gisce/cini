67b1ddb8136746622d8131182ce73b9113f05e4c
Axel Simón <asimon@gisce.net>
2016-05-03 10:27:21 +0200

    Implementados los parámetros y el CINI del CT

--- cini/models.py
+++ cini/models.py
@@ -260,9 +260,8 @@ class Transformador(Base):
 
     @property
     def cini(self):
-        """"Asigna los valores de cada elemento del CINI
-            según los valores de la tensión, situación, potencia
-            y estado.
+        """Obtiene el CINI del transformador
+        :returns :py:class:`CINI`
         """
         c = CINI()
         c.positions[1] = '2'
@@ -318,11 +317,20 @@ class CentroTransformador(Base):
     """
 
     def __init__(self):
+        self.tension_p = None
+        """Tensión primaria en kV
+        """
+        self.tension_s = None
+        """Tensión secundaria en kV
+        """
+        self.reparto = True
+        """Indica si el CT es de reparto o reflexión
+        """
         self.tension = None
         """Tension en kV
         """
         self.tipo = None
-        """"Tipo de centro transformador
+        """Tipo de centro transformador
 
             - Intemperie = ``I``
             - Caseta = ``C``
@@ -331,6 +339,137 @@ class CentroTransformador(Base):
             - Móvil = ``M``
         """
         self.transformadores = []
-        """"
-            Lista de transformadores del CT
+        """Lista de transformadores del CT
         """
+
+    @property
+    def potencia_instalada(self):
+        """Potencia instalada en el centro
+
+        Suma la potencia de todos los transformadores que no esten
+        en estado Reserva (R)
+        """
+        potencia = 0
+        for t in self.transformadores:
+            assert isinstance(t, Transformador)
+            if t.estado != 'R':
+                potencia += t.potencia
+        return potencia
+
+    @property
+    def cini(self):
+        """Obtiene el CINI del centro transformador
+        :returns :py:class:`CINI`
+        """
+        c = CINI()
+        c.positions[1] = '2'
+        c.positions[2] = '2'
+
+        if self.tension_p is not None:
+            if 1 <= self.tension_p < 36:
+                c.positions[3] = '4'
+        if self.tension_s is not None:
+            if self.tension_s < 1:
+                c.positions[4] = '5'
+
+        if self.tipo == 'I':
+            c.positions[5] = '1'
+        elif self.tipo == 'C':
+            c.positions[5] = '2'
+        elif self.tipo == 'L':
+            c.positions[5] = '3'
+        elif self.tipo == 'S':
+            c.positions[5] = '4'
+        elif self.tipo == 'M':
+            c.positions[5] = '9'
+
+        if self.tension is not None:
+            if self.tension <= 1:
+                c.positions[6] = 'C'
+            elif self.tension == 3:
+                c.positions[6] = 'D'
+            elif self.tension == 5:
+                c.positions[6] = 'E'
+            elif self.tension == 5.5:
+                c.positions[6] = 'F'
+            elif self.tension == 6:
+                c.positions[6] = 'G'
+            elif self.tension == 6.6:
+                c.positions[6] = 'H'
+            elif self.tension == 10:
+                c.positions[6] = 'I'
+            elif self.tension == 11:
+                c.positions[6] = 'J'
+            elif self.tension == 12:
+                c.positions[6] = 'K'
+            elif self.tension == 13.2:
+                c.positions[6] = 'L'
+            elif self.tension == 15:
+                c.positions[6] = 'M'
+            elif self.tension == 16:
+                c.positions[6] = 'N'
+            elif self.tension == 20:
+                c.positions[6] = 'O'
+            elif self.tension == 22:
+                c.positions[6] = 'P'
+            elif self.tension == 24:
+                c.positions[6] = 'Q'
+            elif self.tension == 25:
+                c.positions[6] = 'R'
+            elif self.tension == 30:
+                c.positions[6] = 'S'
+            elif self.tension == 33:
+                c.positions[6] = 'T'
+
+        if self.transformadores:
+            if len(self.transformadores) == 1:
+                if self.potencia_instalada == 0:
+                    c.positions[7] = 'A'
+                elif 0 < self.potencia_instalada <= 15:
+                    c.positions[7] = 'B'
+                elif 15 < self.potencia_instalada <= 25:
+                    c.positions[7] = 'C'
+                elif 25 < self.potencia_instalada <= 50:
+                    c.positions[7] = 'D'
+                elif 50 < self.potencia_instalada <= 100:
+                    c.positions[7] = 'E'
+                elif 100 < self.potencia_instalada <= 160:
+                    c.positions[7] = 'F'
+                elif 160 < self.potencia_instalada <= 250:
+                    c.positions[7] = 'G'
+                elif 250 < self.potencia_instalada <= 400:
+                    c.positions[7] = 'H'
+                elif 400 < self.potencia_instalada <= 630:
+                    c.positions[7] = 'I'
+                elif 630 < self.potencia_instalada <= 1000:
+                    c.positions[7] = 'J'
+                elif 1000 < self.potencia_instalada <= 1250:
+                    c.positions[7] = 'K'
+            elif len(self.transformadores) == 2:
+                if self.potencia_instalada <= 2 * 15:
+                    c.positions[7] = 'L'
+                elif 2 * 15 < self.potencia_instalada <= 2 * 25:
+                    c.positions[7] = 'M'
+                elif 2 * 25 < self.potencia_instalada <= 2 * 50:
+                    c.positions[7] = 'N'
+                elif 2 * 50 < self.potencia_instalada <= 2 * 100:
+                    c.positions[7] = 'O'
+                elif 2 * 100 < self.potencia_instalada <= 2 * 160:
+                    c.positions[7] = 'P'
+                elif 2 * 160 < self.potencia_instalada <= 2 * 250:
+                    c.positions[7] = 'Q'
+                elif 2 * 250 < self.potencia_instalada <= 2 * 400:
+                    c.positions[7] = 'R'
+                elif 2 * 400 < self.potencia_instalada <= 2 * 630:
+                    c.positions[7] = 'S'
+                elif 2 * 630 < self.potencia_instalada <= 2 * 1000:
+                    c.positions[7] = 'T'
+                elif 2 * 1000 < self.potencia_instalada <= 2 * 1250:
+                    c.positions[7] = 'U'
+        else:
+            if self.reparto:
+                c.positions[7] = 'V'
+            else:
+                c.positions[7] = 'Z'
+
+        return c
