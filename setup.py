55a5b9c29756f3063f1342a2bbfe6c3556e8c7ba
Axel Simón <asimon@gisce.net>
2016-09-21 11:28:47 +0200

    Bump to v0.2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='cini',
-    version='0.2.2',
+    version='0.2.3',
     packages=setuptools.find_packages(),
     url='https://github.com/gisce/cini',
     license='MIT',
