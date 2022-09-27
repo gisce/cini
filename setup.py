357575b0ca82c2fa7193be1f95b879902c0c527d
tinogis <afita@gisce.net>
2022-09-27 08:36:06 +0200

    Bump to v0.4.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='cini',
-    version='0.3.1',
+    version='0.4.0',
     packages=setuptools.find_packages(),
     url='https://github.com/gisce/cini',
     license='MIT',
