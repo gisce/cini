673c6b71047645844c20c95e25b03d334a74194a
Vilaboa <mvilaboa@gisce.net>
2025-05-13 11:19:32 +0200

    Bump to v0.7.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='cini',
-    version='0.7.0',
+    version='0.7.1',
     packages=setuptools.find_packages(),
     url='https://github.com/gisce/cini',
     license='MIT',
